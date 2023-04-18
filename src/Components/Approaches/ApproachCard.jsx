ed52f66155bb3b288875ac94f6b0b944e8f174f5
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2023-04-18 15:59:57 +0600

    added

--- src/Components/Approaches/ApproachCard.jsx
+++ src/Components/Approaches/ApproachCard.jsx
@@ -9,7 +9,7 @@ function ApproachCard({ data }) {
       onMouseLeave={() => setActive("")}
       onMouseEnter={() => setActive(`${data?._id}`)}
       onClick={() => navigate(`service/${data?._id}`)}
-      className={` md:h-48 bg-[#00172f] sm:border md:border xl:border-0 z-50 p-4    hover:shadow-lg hover:custom-shadow hover:scale-105 duration-500 py-14  flex flex-col  hover:border-none border-blue-900 
+      className={` md:h-48 bg-[#00172f] sm:border md:border xl:border-0 z-50 p-4  hover:shadow-lg hover:custom-shadow hover:scale-105 duration-500 py-14  flex flex-col  hover:border-none border-blue-900 
       ${data?._id == 1 && "xl:border-r  xl:border-b "}
       ${data?._id == 2 && "  xl:border-b"}
       ${data?._id == 3 && "xl:border-r  "}
@@ -40,6 +40,7 @@ function ApproachCard({ data }) {
               src={ArrowRight}
               alt=""
             /> */}
+            {/* not need */}
           </Link>
         )}
       </div>
